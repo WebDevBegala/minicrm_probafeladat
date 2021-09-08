19135ef6d096641801a74bbc584691af42c13aeb
Begala Gergő <begala.g1999@gmail.com>
2021-09-08 03:23:21 +0200

    change api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'http://localhost:3000/';
+const API_URL = 'https://storeapitest123.herokuapp.com/';
 
 export const fetchStoreDetail = (params) => axios.get(`${API_URL}store`,{params});
 export const fetchComments = (params) => axios.get(`${API_URL}comments`,{params});
