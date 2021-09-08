91dd085daaf26ed24dd162f8459af425094b480f
Begala Gergő <begala.g1999@gmail.com>
2021-09-08 03:08:20 +0200

    fix eslint

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 
 mongoose.connect(config.mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = db;
--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const StoreController = require('../controllers/storeController')
+const StoreController = require('../controllers/storeController');
+
 router.get('',(req,res)=>{
     StoreController.get_stores(req,res);
 })
